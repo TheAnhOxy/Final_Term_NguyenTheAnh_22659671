e2cc01f08f513b9ed2e18905238bcfbb392faa89
Thế Anh Oxy <139890192+TheAnhOxy@users.noreply.github.com>
2025-11-15 09:40:39 +0700

    feat(Q1): init expo project and sqlite setup for habit tracker

---
+++ FinalTerm_22659671_NguyenTheAnh/metro.config.js
@@ -0,0 +1,14 @@
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = withNativeWind(config, { input: "./src/global.css" });
+config.resolver.assetExts.push("wasm");
+config.server.enhanceMiddleware = (middleware) => {
+  return (req, res, next) => {
+    res.setHeader("Cross-Origin-Embedder-Policy", "credentialless");
+    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+    middleware(req, res, next);
+  };
+};
